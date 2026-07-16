d60fca8efd8c83eaef9fb4c9c7116ba4dd231957
Pavan Kalyan <pavankalyankanugonda@gmail.com>
2026-07-16 11:43:59 +0530

    3c

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "ci-cd pipeline is working fine narsingu Bety" });
+  res.json({ message: "ci-cd pipeline is working fine error pom kalyan Betey" });
+
 });
 
 app.listen(port, () => {
